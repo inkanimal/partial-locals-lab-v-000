dff8bb4a286d0f6a73dc21ce25bf917c9c82b90b
geoffrey woodcock <theatomproject@gmail.com>
2018-10-30 20:21:49 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,8 +21,11 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.search(params[:query])
-    render 'index'
+    @students = Student.all
+    if params[:search]
+      @students = Student.search(params[:search])
+      redirect_to 'index'
+    end
   end
    
   def student_params
